71ed78fbd8cdc1e90c12474f9123ac517e922f79
shravan809 <dasari.shravan1996@gmail.com>
2024-09-13 17:11:58 +0530

    added files

---
+++ datatypes.js
@@ -0,0 +1,95 @@
+/* Data types:
+
+1.premitive data type
+2.non-premitive data type
+
+1.premitive data type:
+
+1.string
+2.Number
+3.Big Int
+4.Null
+5.Undefined
+6.symbol
+7.Boolean
+
+2.Non - premitive data type:
+
+1.Array
+2.Objects
+
+ */
+
+// 1.string:
+
+/* var firstname = 'shravan'
+lastname = 'dasari'
+console.log(firstname,typeof firstname)
+console.log(lastname, typeof(lastname))
+ */
+
+// output:
+// shravan string
+// dasari string
+
+// 2.Number:
+
+/* let lenght = 24.9
+width = 15
+console.log(typeof lenght,lenght)
+console.log(typeof width,width) */
+
+// output:
+// number 24.9
+// number 15
+
+// 3.Bit Int:
+
+/* phone = 9147852369
+exp = 92345678974185296332124454485464984465131568468561320351654684681315646484561321516466551651848n
+console.log(typeof phone, phone)
+console.log(typeof exp, exp) */
+
+// output:
+// number 9147852369
+// number 9.234567897418529e+94
+
+// 4.Null:
+
+/* const email = null
+
+console.log(typeof email,email) */
+// output:
+// object null
+
+// 5.Undefined:
+
+/* let age;
+
+console.log(typeof age, age)
+age = 27
+console.log(typeof age, age)
+
+output:
+undefined undefined
+number 27 */
+
+// 6.symbol:
+/* 
+var address = Symbol('3-5,bahadurpally')
+
+console.log(typeof address, address) */
+
+// output:
+// symbol Symbol(3-5,bahadurpally)
+
+// 7.Boolean
+/* 
+var gender = true
+
+document.writeln(gender, '<br>', typeof gender)
+
+output:
+true
+boolean */
+
---
+++ variable.js
@@ -0,0 +1,48 @@
+//var:
+/*var product='realme'
+
+var product = 'vivo'
+
+console.log(product)*/
+
+//let:
+
+/*let student='shravan'
+
+let student='vamshi'
+
+console.log(student)*/
+
+//const:
+/* 
+const emp='shravan'
+
+const emp='vamshi'
+
+console.log(emp) */
+
+// scope:
+
+
+/*function example() {
+    var x = 10;
+    console.log(x); 
+}
+example()
+
+a=27
+console.log(a); 
+
+
+if (true) {
+    let y = 10;
+    console.log(y); 
+}
+console.log(a); */
+
+
+var product='realme'
+
+let product = 'vivo'
+
+console.log(product)
