2ba3e261acfa6f4e6ebd2cdeafc9be014690cf69
shravan809 <dasari.shravan1996@gmail.com>
2024-10-04 17:48:45 +0530

    task added

---
+++ task_04-10.js
@@ -0,0 +1,101 @@
+/* 
+Exponentiation: 
+--------------- 
+1.How do you find the square of 4 using Exponentiation? 
+
+let a = 4;
+console.log(a**2)
+//16 
+*/
+
+/*
+2.What is 2 ** -2? what will be the output? 
+
+let a = 2
+console.log(a ** -2)
+//0.25
+*/
+
+/*
+3.What is (-2) ** 3? 
+
+console.log((-2) ** 3)
+//-8
+*/
+/*
+4.What is 8 ** (1/3)? 
+
+console.log(8**(1/3))//2*/
+
+/*
+5.What is 1.5 ** 2? 
+
+// console.log(1.5 ** 2)
+//2
+
+
+//6.Write more examples using exponent similar to above questions?
+
+console.log(3.2 ** 2)//10.24
+console.log(8 ** 3)//512
+console.log(4 ** 6)//4096*/
+
+/*
+Logical operator: 
+----------------- 
+1.write a code using the logical operators AND (&&) and OR (||) with all possible combinations of true and false values. 
+
+example: */
+console.log(true && true) //true
+console.log(true && false)//false
+console.log(false && true)//false
+console.log(false && false)//false
+
+/*
+comparison operator: 
+-------------------- 
+1. write a code using the all comparison operators (==,===,!=,!==,<=,>=,<,>)? 
+example:  
+let a=2 
+let b=4 
+console.log(a+2>b-2) 
+
+Following are Java Script tasks on loops and conditional statements,if statements 
+
+1.Write a program that checks if a number is positive, negative, or zero. 
+
+2.Write a program that assigns a grade (A, B, C, D, F) based on a score using nested if. 
+
+3.Develop a program that determines if a number is even or odd and whether it is within a certain range (1 to 100) 
+
+4.Write a program that checks if a person is eligible to vote based on their age and citizenship status using nested if. 
+
+5.Write a program that determines if a character is a vowel or consonant? 
+
+for loop: 
+--------- 
+1.Write a for loop to compute the sum of all integers from 1 to 50. 
+2.Write a for loop that prints the first 10 multiples of a given number n? 
+3.Write a for loop to print all prime numbers between 1 and 100. 
+4.Use a for loop to compute the sum of squares of the first 10 positive integers. 
+5.Write a program that prints the sum of each pair of numbers between 1 and 2 using nested for loop? 
+
+Nested loops: 
+--------------- 
+1.Write a program that calculates the sum of all i**j values for i and j ranging from 1 to 3(using exponent). 
+2.Write a program that counts and prints pairs (i, j) between 1 and 4 where one number is even and the other is odd. 
+3.Write a program that prints all pairs (i, j) between 1 and 5 where the difference between i and j is 2. 
+4.Write a program where the outer loop runs from 5 to 1, and the inner loop runs from 1 to 3, printing each pair (i, j). 
+5.Write a program that finds pairs of numbers (i, j) between 1 and 5 where both numbers are prime. 
+
+6.Write a program that checks and prints when i is equal to or greater than j in nested loop running from 1 to 5 range. 
+
+7.Write a program that counts the total number of iterations performed in nested loops where the outer loop runs from 1 to 3 and the inner loop runs from 1 to 2. 
+
+8.Write a program that checks if the sum of pairs (i, j) between 1 and 4 is a prime number. 
+
+9.Write a program that finds pairs (i, j) from 1 to 3 where the sum i + j is greater than the product i * j. 
+
+10.Write a program that prints pairs (i, j) from 1 to 3 where the product i * j is even. 
+
+*/
