29ac209659d1c9291677be70e5bedb489c8556c7
shravan809 <dasari.shravan1996@gmail.com>
2024-10-07 19:08:56 +0530

    added

---
+++ task_07-10.js
@@ -0,0 +1,60 @@
+/* 1.Count how many times a letter occurring in a string
+         input : sri veni pogula 
+         Output : { s : 1, r : 1, i : 2, v : 1,                 e : 1, n : 1, p: 1, o:1, g:0, u:0, l:0, a:0 }
+2. replace all Vowels(By taking one sample text json file) in object body value (string)  with  #
+ 
+3.Take one array and add first all even numbers and second all odd numbers in a digit.
+         Input:[2435,982,5642,1232,87]
+         Output:[68,109,125,44,87]
+ 
+4.Take One Array and do the process as if the number consists more than 3 digits To check the coditions as
+=>First and last digits are both even or odd no need to perform any operation the same number will return otherwise the first and last numbers are one is even and one is odd it will be added and remaining number is append it.
+    Input: [287, 7342, 934757,66542,75,765765]
+    Output: [98,97,16811,8105,12,121212]
+ 
+5.Take One Array Consists of all digits like single digit numbers,double,triple and more in that you have follows some  rules:-
+  =>if it is one digit even or odd number we can add 0 in absence place
+=>if it is 2 or more digits we have to count all even and odd numbers.
+   Input:[5,7,8,6,25,18,19,27,33,88,754,484,374,987,8765,7645,8994,6548,932157,48327,9875432]
+  Output:[10,10,01,01,11,11,20,11,20,02,21,03,21,21,22,22,22,13,51,23,43]
+ 
+6.let a=3;
+  let array1 = [0,1,2,3,4,5,6,7,8,9];
+  output = [[0,1,2], [3,4,5], [6,7,8], [9]]
+ 
+  let b =5;
+  output = [[0,1,2, 3,4],[5, 6,7,8,9]]
+ 
+
+7.work on  this task 
+for the given array Remove any negative numbers from the array,
+Double each remaining number,
+Check if every number is even after the transformation,
+Return the sum of the transformed numbers if all are even, otherwise return null.
+ 
+Input:- let numbers = [12, -7, 5, 8, -10, 20];
+Expected Output: 90
+ 
+8.Taking For loop for 1 to 50 We have to count the total number of 0's,1's,2's,3's,4's,5's,6's,7's,8's,9's
+ 
+9.Taking One Array And follow the rules
+=>if it is one digits we can add 0
+=>if it is consists 2 digits we can swap
+=>if it is 3 or more digits we can swap upto the digits and the last digit may constant.
+ 
+Input:-[8,59,738,9645,87657,123456789]
+Output:-[80, 95, 378, 4695, 56787, 876543219]
+ 
+10.Combination Sum
+Objective: Given an array of distinct integers and a target number, find all unique combinations of integers that sum up to the target.
+console.log(combinationSum([2, 3, 6, 7], 7));
+// Output: [[7], [2, 2, 3]]
+Number of Islands
+Objective: Given a grid of '1's (land) and '0's (water), count the number of islands.
+console.log(numIslands([
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","0","0"]
+])); 
+//Output: 1 */
