f89f2aab1cb968e161505035a45b145703d45db5
shravan809 <dasari.shravan1996@gmail.com>
2024-10-14 10:18:16 +0530

    added task

--- task_07-10.js
+++ task_07-10.js
@@ -81,9 +81,10 @@ for (i = 0; i < input.length; i++) {
 	} else {
 		output.push(parseInt(s))
 	}
-	
 }
 console.log(output)
+
+
  /*
 5.Take One Array Consists of all digits like single digit numbers,double,triple and more in that you have follows some  rules:-
   =>if it is one digit even or odd number we can add 0 in absence place
@@ -91,6 +92,11 @@ console.log(output)
    Input:[5,7,8,6,25,18,19,27,33,88,754,484,374,987,8765,7645,8994,6548,932157,48327,9875432]
   Output:[10,10,01,01,11,11,20,11,20,02,21,03,21,21,22,22,22,13,51,23,43]
  
+ 
+ 
+ 
+ 
+
 6.let a=3;
   let array1 = [0,1,2,3,4,5,6,7,8,9];
   output = [[0,1,2], [3,4,5], [6,7,8], [9]]
